ef5437c4bd338b3acac72c4655b029ee4909f9f6
ochen1 <59662605+ochen1@users.noreply.github.com>
2020-08-15 18:19:08 -0600

    Create NitroAdblock.plugin.js

---
+++ NitroAdblock/NitroAdblock.plugin.js
@@ -0,0 +1,62 @@
+/**
+ * @name NitroAdblock
+ * @version 1.0.0
+ * @description Removes the Discord ad for Nitro.
+ *
+ * @website https://github.com/ochen1/BetterDiscordPlugins
+ * @source https://raw.githubusercontent.com/ochen1/BetterDiscordPlugins/master/NitroAdblock/NitroAdblock.plugin.js
+ * @invite 7PZcsVK
+ * @donate https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=Z87WKG5G9JBYL&item_name=BetterDiscord+Plugin+-+Nitro+Adblock+Donation&currency_code=CAD&source=url
+ * @authorLink https://github.com/ochen1
+ * @authorId 694234706430001222
+ */
+
+module.exports = class NitroAdblock {
+  getName () {
+    return 'Nitro Adblock'
+  }
+
+  getDescription () {
+    return 'Prevents Nitro ads from existing in the DMs list.'
+  }
+
+  getVersion () {
+    return '1.0.0'
+  }
+
+  getAuthor () {
+    return 'idontknow'
+  }
+
+  load () {
+
+  }
+
+  isAd (channel) {
+    return (channel.innerText === 'Nitro' && channel.href === 'https://discord.com/store')
+  }
+
+  removeAds () {
+    for (const privateChannel of document.getElementById('private-channels').querySelectorAll('.da-channel')) {
+      if (this.isAd(privateChannel)) {
+        privateChannel.remove()
+      }
+    }
+  }
+
+  start () {
+    this.removeAds()
+  }
+
+  stop () {
+
+  }
+
+  observer (changes) {
+    for (const addedNode of changes.addedNodes) {
+      if (this.isAd(addedNode)) {
+        addedNode.remove()
+      }
+    }
+  }
+}
